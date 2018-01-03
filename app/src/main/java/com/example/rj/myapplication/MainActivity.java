002f24f456932847de8ea38c811f21e030c9df0d
Rj90029 <rj90029@gmail.com>
2018-01-03 22:21:39 +0530

    MHR Files

---
+++ app/src/main/java/com/example/rj/myapplication/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.rj.myapplication;
+
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GestureDetectorCompat gestureObject;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gestureObject = new GestureDetectorCompat(this, new LearnGesture());
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        this.gestureObject.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    class LearnGesture extends  GestureDetector.SimpleOnGestureListener {
+
+
+        @Override
+        public boolean onFling(MotionEvent event1, MotionEvent event2,
+                               float velocityX, float velocityY) {
+
+            if(event2.getX()>event1.getX()) {
+
+            }
+            else if(event2.getX()<event1.getX()) {
+                setContentView(R.layout.about);
+            }
+
+            return true;
+        }
+    }
+}
